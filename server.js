579053db13292286734fdd4ca35a42e425752463
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-05-01 12:49:38 -0700

    got server wired up to meetup api, verified data is accurate

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const superagent = require('superagent');
-// test
+
+//token: ac725033530724b6e295ef5d4352;
+
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 // const TOKEN = process.env.TOKEN;
+
+app.use(express.static('./public'));
+app.use(cors());
+
+const MEETUP_API_KEY = process.env.MEETUP_API_KEY;
 
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -16,10 +23,27 @@ client.connect();
 client.on('error', err => console.error(err));
 
 // app.get('/', (req, res) => res.send());
+// app.get('/', (req, res) => {
+//   client.query('SELECT * FROM meetups')
+//     .then(results => res.send(results.rows));
+//   // .catch(console.error);
+// });
+
+app.get('/meetup/*', (req, res) => {
+  console.log('routing a request to Meetup');
+  // const url = `https://api.github.com/${req.params[0]}`;
+  const url = 'https://api.meetup.com/find/upcoming_events?photo-host=public&page=10&text=tech&sig_id=179434442&radius=5&sig=f5a46d787e0f4bd9bd1fef00917db80ffb52fdd3';
+  superagent.get(url)
+    // .set('Authorization', process.env.GITHUB_TOKEN)
+    .then(meetups => {
+      console.log(meetups.body)
+      res.send(meetups)
+    })
+    .catch(error => console.log('error', error.message));
+});
+
 app.get('/', (req, res) => {
-  client.query('SELECT * FROM meetups')
-    .then(results => res.send(results.rows));
-  // .catch(console.error);
+  res.sendFile('index.html')
 });
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
